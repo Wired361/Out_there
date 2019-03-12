0c1e41c5480b1640fda90657d983ceb978ebf5ee
Justin Parker <justin.parker.12@gmail.com>
2019-03-12 09:25:58 -0500

    moved created table to header

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,14 @@
-// function that creates a table at an id (probably placeholder)
+// function that creates a table header at #placeholder
 function createTable(columns, id) {
   var table = d3
       .select(id)
+      .attr('class', 'container-fluid')
       .append('table')
       .attr('id', 'dataTable')
-      .attr('class', 'table table-hover table-dark table-striped'),
+      .attr(
+        'class',
+        'table table-hover table-dark table-striped table-responsive'
+      ),
     thead = table.append('thead');
 
   // append the header row with filtered columns
@@ -19,62 +23,67 @@ function createTable(columns, id) {
       return headText.split(/(?=[A-Z])/).join(' in ');
     })
     .attr('scope', 'col')
-    .attr('class', 'text-capitalize');
+    .attr('class', 'text-capitalize sticky');
   return table;
+}
+
+// Function for creating table with all the data
+function allData() {
+  // create a row for each object in the data
+  var tbody = d3.select('#dataTable').append('tbody'),
+    rows = tbody
+      .selectAll('tr')
+      .data(data)
+      .enter()
+      .append('tr'),
+    cellData = rows
+      .selectAll('td')
+      .data(function(row) {
+        return columns.map(function(key) {
+          if (key == 'state' || key == 'country') {
+            row[key] = row[key].toUpperCase();
+            console.log(row[key]);
+          }
+
+          return { key: key, value: row[key] };
+        });
+      })
+      .enter()
+      .append('td')
+      .attr('class', 'text-truncate text-capitalize')
+      .text(function(tableData) {
+        return tableData.value;
+      });
+
+  return tbody;
+}
+
+// Function for header reformatting
+function headerFormat() {
+  var headTitle = d3.select('#header').attr('class', 'py-5');
+  return headTitle;
 }
 // grab keys from first data.js object and create an array called columns
 var columns = Object.keys(data[0]);
 var phid = '#placeholder';
-//create a function that filters based on form input (maybe a map?)
-d3.select('#exploreButton').on('click', function() {
-  d3.event.preventDefault();
-  d3.select('#exploreButton').attr('class', 'd-none');
-  d3.select('#allButton').attr('class', 'btn btn-light btn-lg');
-  createTable(columns, phid);
-});
 
-// function to create full table
-function rows(key) {
-  data.map(dataobj => dataobj.key);
-}
-
-function allTable() {
-  // create a row for each object in the data
-  var tbody = d3.select('#dataTable').append('tbody');
-  var rows = tbody
-    .selectAll('tr')
-    .data(data)
-    .enter()
-    .append('tr');
-
-  // create a cell in each row for each column
-  var cells = rows
-    .selectAll('td')
-    .data(function(row) {
-      return columns.map(function(key) {
-        if (key == 'state' || key == 'country') {
-          row[key] = row[key].toUpperCase();
-          console.log(row[key]);
-        }
-
-        return { key: key, value: row[key] };
-      });
-    })
-    .enter()
-    .append('td')
-    .attr('class', 'text-truncate text-capitalize')
-    .text(function(tableData) {
-      return tableData.value;
-    });
-
-  return tbody;
-}
-// apply a map to the data looping the previous function over all objects in data.js
+// Event listener for create full table button
 d3.select('#allButton').on('click', function() {
   d3.event.preventDefault();
-  d3.select('#exploreButton').attr('class', 'd-none');
-  d3.select('#allButton').attr('class', 'btn btn-light btn-lg');
-  allTable();
+  d3.select('#allButton').attr('class', 'd-none');
+  d3.select('#filterForm').attr(
+    'class',
+    'd-flex form-inline justify-content-center'
+  );
+  d3.select('#searchButton').attr('class', 'btn btn-light btn-small ml-2');
+  headerFormat();
+  createTable(columns, phid);
+  allData();
+});
+
+// Swap Buttons for search input
+d3.select('#searchButton').on('click', function() {
+  d3.event.preventDefault();
 });
 
 data.map(row => row.pair);
